8e33552b58017bd9ca7e22a7620f224d0fe783d0
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-08-21 15:41:13 +0530

    done, now let's do some minor changes

--- Month 2/Week 1/JS/app.js
+++ Month 2/Week 1/JS/app.js
@@ -1,13 +1,19 @@
 const getColor = () => {
+
     const randomNumber = Math.floor(Math.random() * 16777215);
     const randomCode = "#" + randomNumber.toString(16);
-    const pallet1 = document.getElementById("pallet1");
     document.body.style.backgroundColor = randomCode;
-    document.getElementById("pallet1").innerHTML = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
 
+    navigator.clipboard.writeText(randomCode)
 }
-document.getElementById("butn").addEventListener(
-    "click"
-    
+
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
 )
+
+
+
 getColor();
