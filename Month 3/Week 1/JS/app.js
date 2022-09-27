f0ab1d2dd9c979eb26627e064f1037d6f1374faf
Pronoy Bhattacharya <peonoyb07@gmail.com>
2022-09-27 16:18:29 +0530

    month 3 added

---
+++ Month 3/Week 1/JS/app.js
@@ -0,0 +1,19 @@
+const getColor = () => {
+
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomCode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+
+    navigator.clipboard.writeText(randomCode)
+}
+
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+
+
+getColor();
---
+++ Month 3/Week 4/README.md
@@ -0,0 +1,3 @@
+To see the output go to week's folder(inside every month's folder), there's a Readme file, it contains everything
+<br><br>
+Month → Week → Readme
